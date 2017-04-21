2017de4c02b8c83197404bf9cd999402ace0cd65
Kelly Innes <kinnes@azavea.com>
2017-04-21 13:38:50 -0400

    Add why-did-you-update library for React

--- app/js/Main.jsx
+++ app/js/Main.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
+import { whyDidYouUpdate } from 'why-did-you-update';
 
 import '../sass/main.scss';
 import createStoreWithMiddleware from './store';
+import { isDevelopment } from './constants';
 import mainReducer from './reducer';
 import App from './components/App.jsx';
 
 const store = createStoreWithMiddleware(mainReducer);
+
+if (isDevelopment) {
+    whyDidYouUpdate(React);
+}
 
 render(
     <Provider store={store}>
