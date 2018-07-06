63f7932dcd828e81b2769fcb82ed1b9d5879fff3
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2018-07-06 18:12:41 -0400

    Add comments to Paint class to help follow along

--- ingest/src/main/scala/LandCoverPaint.scala
+++ ingest/src/main/scala/LandCoverPaint.scala
@@ -14,28 +14,45 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.serializer.KryoSerializer
 
 object LandCoverPaint {
+  // Number of Spark Partitions. Recommended to go 5-10x the number of cores
   val SparkPartitions = 50
+
+  // Tile Size in pixels. 256 is pretty standard for visual tiles
   val TileSize = 256
+
+  // Since the data is integral, we use Nearest Neighbor resampling method.
+  // For float values, we prefer Bilinear.
   val ResampleMethod = NearestNeighbor
+
+  // Maximum Zoom for which to pain the layers. Each zoom level is an order
+  // of magnitude larger than the last. 13 has a scale of 1:70K, appropriate
+  // for representing villages and suburbs.
   val MaxZoom = 13
+
+  // Layer Name which will be used for reading the ingested data layer as well
+  // as the directory for tiles to be written in.
   val LayerName = "nlcd-pennsylvania"
 
+  // Path to local GeoTIFF, used for reading the color ramp
   val localGeoTiffPath = new java.io.File(
     new java.io.File(".").getCanonicalFile,
     "land-cover-data/geotiff/nlcd_pa.tif"
   ).getAbsolutePath
 
+  // Path to local ingested data, used for reading the cell values
   val localCatalogPath = new java.io.File(
     new java.io.File(".").getCanonicalFile,
     "land-cover-data/catalog"
   ).getAbsolutePath
 
+  // Path to local tiles directory, used for writing the tile images
   val localTilesPath = new java.io.File(
     new java.io.File(".").getCanonicalFile,
     "land-cover-data/tiles"
   ).getAbsolutePath
 
   def main(args: Array[String]): Unit = {
+    // Initialize Spark
     val conf = new SparkConf()
       .setIfMissing("spark.master", "local[*]")
       .setAppName("Paint PA Land Cover Tiles")
@@ -45,22 +62,23 @@ object LandCoverPaint {
     implicit val sc = new SparkContext(conf)
 
     try {
-      // Read ColorMap from original GeoTiff
+      // Read color map from original GeoTIFF
       val geotiff = SinglebandGeoTiff(localGeoTiffPath)
       val colorMap = geotiff.options.colorMap.get
 
+      // Initialize source layer from the local catalog
       val layerId = LayerId(LayerName, 0)
       val layerReader = FileLayerReader(localCatalogPath)
-
       val sourceLayer =
         layerReader.read[SpatialKey, Tile, TileLayerMetadata[SpatialKey]](
           layerId,
           SparkPartitions
         )
 
+      // Set zoomed layout scheme with projection and tile size
       val layoutScheme = ZoomedLayoutScheme(WebMercator, TileSize)
 
-      // Reproject the layer to WebMercator for painting
+      // Reproject the source layer to WebMercator for painting
       val LayoutLevel(_, layoutDefinition) = layoutScheme.levelForZoom(MaxZoom)
       val (_, webMercatorLayer) =
         sourceLayer.reproject(
@@ -73,15 +91,19 @@ object LandCoverPaint {
 
       val path = s"$localTilesPath/$LayerName/{z}/{x}/{y}.png"
 
+      // Pyramid the reprojected layer into as many zoom levels as specified,
+      // given the layout scheme and resampling method
       Pyramid.levelStream(
         webMercatorLayer,
         layoutScheme,
         MaxZoom,
-        NearestNeighbor
+        ResampleMethod
       ).foreach { case (z, levelLayer) =>
+        // For each zoom level layer, find the spatial key to z/x/y path
         val paintedLayerId = LayerId(LayerName, z)
         val keyToPath = SaveToHadoop.spatialKeyToPath(paintedLayerId, path)
 
+        // Paint the layer values with the color map from above and save
         levelLayer
           .mapValues(_.renderPng(colorMap).bytes)
           .saveToHadoop(keyToPath)
